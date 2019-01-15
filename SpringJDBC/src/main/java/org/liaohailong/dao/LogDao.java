0a9a8400eec0124eead01a745cc567807413efd7
liaohailong <http://112.74.67.60:8080/liaohailong/htks-android.git>
2019-01-15 11:30:31 +0800

    merge

--- SpringJDBC/src/main/java/org/liaohailong/dao/LogDao.java
+++ SpringJDBC/src/main/java/org/liaohailong/dao/LogDao.java
@@ -3,8 +3,6 @@ package org.liaohailong.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class LogDao {
